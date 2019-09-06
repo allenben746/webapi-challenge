5c4b0a052ef14878591992cffcb3c7a79980471f
Ben <allenben@protonmail.com>
2019-09-06 11:25:56 -0500

    Linked routes via server.js

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express')
 
 const server = express();
-server.use(express.json())
-server.use(logger)
+server.use(express.json());
+server.use(logger);
 
+//Routes
+server.use('/projects', projectsRouter)
+server.use('/actions', actionsRouter)
+//Sanity test
 server.get('/', (req, res) => {
   res.status(200).json({message: 'api running!'})
 })
+
 //custom middleware
 
 function logger(req, res, next) {
