8c4f7a625314f30b3d9eb7251ec48f3375a1cef1
Ben <allenben@protonmail.com>
2019-09-06 11:21:22 -0500

    Added GET, POST, & PUT requests on Projects

--- projects/projectsRoute.js
+++ projects/projectsRoute.js
@@ -1,10 +1,12 @@
 const express = require('express');
+
 const projectsDB = require('../data/helpers/projectModel');
+
 const router = express.Router();
 
 //Middleware
 
-function validatePostId(req, res, next) {
+function confirmProjectID(req, res, next) {
     const id = req.params.id
     postDb.getById(Number(id))
         .then(response => {
@@ -13,12 +15,62 @@ function validatePostId(req, res, next) {
                 next();
             }
             else {
-                res.status(400).json({ message: 'Invalid post id'})
+                res.status(400).json({ message: 'ID does not exist - please try again.'})
             }
         })
         .catch(error => {
-            res.status(500).json({ message: 'Cannot fetch post data'})
+            res.status(500).json({ message: 'Error retrieving server data.'})
         })
 };
+
+function confirmProject(req, res, next) {
+    const project = req.body;
+    console.log(project)
+    if(!project.name) {
+        res.status(400).json({message: "Name is required."})
+    } else if(!project.description) {
+        res.status(400).json({message: "Description is required."})
+    } else if(!project){
+        res.status(400).json({message: "Data of project is required."})
+    } else {
+        next();
+    }
+}
+
+//Requests
+
+router.get('/:id',confirmProjectID, (req, res) => {
+    const id = req.params.id;
+    projectsDB.get(id)
+    .then(result => {
+        res.status(200).json(result)
+    })
+    .catch(err => {
+        res.status(500).json({error: err})
+    })
+})
+
+router.post('/', confirmProject, (req, res) => {
+    const project = req.body;
+    projectsDB.insert(project)
+    .then(result => {
+        res.status(201).json(result)
+    })
+    .catch(err => {
+        res.status(500).json({ message: 'Error retrieving server data.'})
+    })
+})
+
+router.put('/:id', confirmProjectID, confirmProject, (req, res) => {
+    const id = req.params.id
+    const project = req.body;
+    projectsDB.update(id, project)
+    .then(result => {
+        res.status(200).json(result)
+    })
+    .catch(err => {
+        res.status(500).json({ message: 'Error retrieving server data.'})
+    })
+})
 
 module.exports = router;
